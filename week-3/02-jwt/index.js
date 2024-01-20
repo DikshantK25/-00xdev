d5505415420541820839bad4d4d82c9761125606
Dikshant Koriwar <dikshantkoriwar29@gmail.com>
2024-01-20 15:19:32 +0530

    completed

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,5 @@
-const jwt = require('jsonwebtoken');
-const jwtPassword = 'secret';
-
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret";
 
 /**
  * Generates a JWT for a given username and password.
@@ -13,9 +12,29 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+
+const zod = require("zod");
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
+// We then use the .parse() method to validate the data against the schema. If the data is valid, the console.log() statement will be executed. Otherwise, the console.error() statement will be executed with the validation error. It’s like having X-ray vision for your data!
+
+// safeParse() is a method in Zod that allows you to parse data safely without throwing an error. Instead of throwing an error when the data is invalid, safeParse() returns a ZodParsedType<T> object that contains both the parsed data and a boolean success property that indicates whether the parsing was successful
+
 function signJwt(username, password) {
-    // Your code here
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    if (!usernameResponse.success || !passwordResponse.success) {
+        return null;
+    }
+
+    const signature = jwt.sign({
+        username
+    }, jwtPassword);
+
+    return signature;
 }
+
 
 /**
  * Verifies a JWT using a secret key.
@@ -25,10 +44,18 @@ function signJwt(username, password) {
  *                    Returns false if the token is invalid, expired, or not verified
  *                    using the secret key.
  */
-function verifyJwt(token) {
-    // Your code here
-}
 
+//impoetant to rember that verfy should be try-catch block format   
+function verifyJwt(token) {
+    let ans = true;
+    try {
+       jwt.verify(token, jwtPassword);
+    } catch(e) {
+       ans = false;
+    }
+    return ans;
+}
+  
 /**
  * Decodes a JWT to reveal its payload without verifying its authenticity.
  *
@@ -37,9 +64,14 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+    // true, false
+    const decoded = jwt.decode(token);
+    if (decoded) {
+        return true;
+    } else {
+        return false;
+    }
 }
-
 
 module.exports = {
   signJwt,
